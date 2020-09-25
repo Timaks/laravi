caab2af5c33b83312efc3e0454d80b65a6490355
Aksinya <aksinya2016@gmail.com>
2020-09-25 20:42:07 +0300

    adddd

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squadfree Bootstrap Theme Demo</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i" type="text/css" media="all">
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/normalize.css">
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/fontello.css">
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/style.css">
+</head>
+
